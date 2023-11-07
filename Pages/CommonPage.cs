05daa638c5f8a51fc9afcd3974c687d9603765d9
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-11-06 22:37:55 -0300

    Fim SpecFlow Simples - Inicio PageObjects

---
+++ Pages/CommonPage.cs
@@ -0,0 +1,23 @@
+
+using OpenQA.Selenium;
+
+namespace Pages
+{
+    public class CommonPage
+    {
+        // Atributos
+        protected IWebDriver driver;
+
+        // Mapeamento dos Elementos comuns a duas ou mais páginas
+        private IWebElement lblTituloSecao => driver.FindElement(By.CssSelector("span.title"));
+        private IWebElement icoCarrinho => driver.FindElement(By.CssSelector("a.shopping_cart_link"));
+
+        // Métodos e Funções
+        // Contrutor
+        public CommonPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+    }
+}
---
+++ Pages/InventoryPage.cs
@@ -0,0 +1,17 @@
+using OpenQA.Selenium;
+
+namespace Pages
+{
+    public class InventoryPage : CommonPage // extends
+    {
+        // Mapeamento dos Elementos
+
+        // Construtor
+        public InventoryPage(IWebDriver driver) : base(driver)
+        {
+
+        }
+        // Ações
+
+    }
+}
